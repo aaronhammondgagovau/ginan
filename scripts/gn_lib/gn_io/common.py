9988513ca27d848901f525a398d7ba10672b9a85
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 21:25:59 +0000

    Adding numeric_owner as keyword arguement

--- scripts/gn_lib/gn_io/common.py
+++ scripts/gn_lib/gn_io/common.py
@@ -76,7 +76,7 @@ def tar_extr(srcpath,destpath):
                 if not is_within_directory(path, member_path):
                     raise Exception("Attempted Path Traversal in Tar File")
         
-            tar.extractall(path, members, numeric_owner) 
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
             
         
         safe_extract(tar, path=destpath)
